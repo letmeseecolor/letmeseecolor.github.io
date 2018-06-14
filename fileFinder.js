942005551a1b1f46301a1513c1f172a7087bcae9
Convidado <silvatavares.bruna@gmail.com>
2018-06-14 16:31:18 -0300

    corrigido formato imagem input

--- fileFinder.js
+++ fileFinder.js
@@ -2,13 +2,18 @@ document.getElementById('picField').onchange = function (evt) {
   var tgt = evt.target || window.event.srcElement, files = tgt.files
 
   if (FileReader && files && files.length) {
-    var fr = new FileReader()
+    if (files[0].type == "image/png" || files[0].type == "image/jpg" ||files[0].type == "image/jpeg" ){
+      var fr = new FileReader()
 
-    fr.onload = function () {
-      img = loadImage(fr.result, renderProcess);
-
+      fr.onload = function () {
+        img = loadImage(fr.result, renderProcess);
+      }
+      fr.readAsDataURL(files[0])
+    }else{
+        infoText.html( "<font color=\"#F00\">O arquivo deve ser uma imagem!</font>" );
+        console.log("ahdua");
+        document.getElementById('picField').value = "";
     }
-    fr.readAsDataURL(files[0])
   }
   hasUpdate = true;
 }
